4b754f622c82c98ad3d743c5742f03233f1ebb5f
zhangjf <zhangjianfei@techno-vision.cn>
2018-10-30 14:04:19 +0800

    @###

--- html/src/main/java/com/example/html/HtmlApplication.java
+++ html/src/main/java/com/example/html/HtmlApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HtmlApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("server.port","8081");
 		SpringApplication.run(HtmlApplication.class, args);
 	}
 }
